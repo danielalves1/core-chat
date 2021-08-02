a88055d1bf50ee68a5e8d970903f6a03b90c60ed
Daniel Alves <danielcamposoficial@gmail.com>
2021-08-02 11:25:52 -0400

    Fix layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,51 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import icon from '../assets/icon.svg';
-import './App.global.css';
+import './App.global.scss';
 import HeaderChat from './components/HeaderChat';
+import HeaderTitle from './components/HeaderTitle';
 import LeftContainer from './components/LeftContainer';
 import RightContainer from './components/RightContainer';
 
 const Hello = () => {
   return (
-    <div
-      style={{
-        width: '100vw',
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'row',
-      }}
-    >
-      <LeftContainer>
-        <ul>
-          <li>
-            <div>Logo</div>
-            <div>Fulano</div>
-          </li>
-        </ul>
-      </LeftContainer>
-      <RightContainer>
-        <HeaderChat
-          data={{
-            photo: 'https://avatars.githubusercontent.com/u/33464352?s=400&v=4',
-            fullname: 'Daniel Alves',
-          }}
-        />
-        <div>
-          <h3>dialog</h3>
-        </div>
-        <div>
-          <h3>footer</h3>
-        </div>
-      </RightContainer>
+    <div className="app--container">
+      <HeaderTitle />
+      <div className="menu--bar">
+        <span>File</span>
+      </div>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'row',
+        }}
+      >
+        <LeftContainer>
+          <ul>
+            <li>
+              <div>Logo</div>
+              <div>Fulano</div>
+            </li>
+          </ul>
+        </LeftContainer>
+        <RightContainer>
+          <HeaderChat
+            data={{
+              photo:
+                'https://avatars.githubusercontent.com/u/33464352?s=400&v=4',
+              fullname: 'Daniel Alves',
+            }}
+          />
+          <div>
+            <h3>dialog</h3>
+          </div>
+          <div>
+            <h3>footer</h3>
+          </div>
+        </RightContainer>
+      </div>
     </div>
   );
 };
---
+++ src/components/HeaderTitle/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './style.scss';
+
+const HeaderTitle = () => {
+  return (
+    <div className="title--bar">
+      <span>Chat</span>
+    </div>
+  );
+};
+
+export default HeaderTitle;
