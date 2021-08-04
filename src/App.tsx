bc2392b50cfc2a923ff15bc0089315f7258452de
Daniel Alves <danielcamposoficial@gmail.com>
2021-08-04 17:24:36 -0400

    Added input search

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 // import icon from '../assets/icon.svg';
-import { Search } from '@material-ui/icons';
+import { DoneAll, Search } from '@material-ui/icons';
 import './App.global.scss';
 import HeaderChat from './components/HeaderChat';
 import HeaderTitle from './components/HeaderTitle';
@@ -100,16 +100,28 @@ const Hello = () => {
           />
           <div className="dialog">
             <div className="received">
-              <span className="message">
-                How the hell am I supposed to get a jury to believe you when I
-                am not even sure that I do?!
-              </span>
+              <div className="content">
+                <span className="message">
+                  How the hell am I supposed to get a jury to believe you when I
+                  am not even sure that I do?!
+                </span>
+                <span className="message--info">
+                  23:59
+                  <DoneAll className="deliver" />
+                </span>
+              </div>
             </div>
             <div className="sent">
-              <span className="message">
-                When you're backed against the wall, break the god damn thing
-                down.
-              </span>
+              <div className="content">
+                <span className="message">
+                  When you're backed against the wall, break the god damn thing
+                  down.
+                </span>
+                <span className="message--info">
+                  23:59
+                  <DoneAll className="deliver" />
+                </span>
+              </div>
             </div>
             <div className="sent">
               <div className="content">
@@ -118,17 +130,7 @@ const Hello = () => {
                 </span>
                 <span className="message--info">
                   23:59
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 15"
-                    width="16"
-                    height="15"
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M15.01 3.316l-.478-.372a.365.365 0 0 0-.51.063L8.666 9.879a.32.32 0 0 1-.484.033l-.358-.325a.319.319 0 0 0-.484.032l-.378.483a.418.418 0 0 0 .036.541l1.32 1.266c.143.14.361.125.484-.033l6.272-8.048a.366.366 0 0 0-.064-.512zm-4.1 0l-.478-.372a.365.365 0 0 0-.51.063L4.566 9.879a.32.32 0 0 1-.484.033L1.891 7.769a.366.366 0 0 0-.515.006l-.423.433a.364.364 0 0 0 .006.514l3.258 3.185c.143.14.361.125.484-.033l6.272-8.048a.365.365 0 0 0-.063-.51z"
-                    />
-                  </svg>
+                  <DoneAll className="deliver" />
                 </span>
               </div>
               <div className="photo">
